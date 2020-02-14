31a1e62d18bdf91f9aa66eb4e6acba85a917b093
Nicholas Gaston <nicholas.gaston@snhu.edu>
2020-02-14 13:26:13 -0500

    Just testing if the commit worked

---
+++ cmake-build-debug/CMakeFiles/Crapsv2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Crapsv2.dir/main.cpp.obj"
+  "Crapsv2.exe"
+  "Crapsv2.exe.manifest"
+  "Crapsv2.pdb"
+  "libCrapsv2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Crapsv2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
