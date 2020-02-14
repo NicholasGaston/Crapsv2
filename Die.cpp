98fbc70362d06f2752ffa1051eae4a247b74b549
Nicholas Gaston <nicholas.gaston@snhu.edu>
2020-02-14 13:32:20 -0500

    Added a new Class Die.cpp and tis associated header

---
+++ Die.cpp
@@ -0,0 +1,5 @@
+//
+// Created by marsb on 2/14/2020.
+//
+
+#include "Die.h"
---
+++ Die.h
@@ -0,0 +1,8 @@
+//
+// Created by marsb on 2/14/2020.
+//
+
+#ifndef CRAPSV2_DIE_H
+#define CRAPSV2_DIE_H
+
+#endif //CRAPSV2_DIE_H
