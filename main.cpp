ebd837abde35c2430c8f8cd32ffd27c6e5045829
Nicholas Gaston <nicholas.gaston@snhu.edu>
2020-02-20 23:59:35 -0500

    Addeed GameLogic C file and its associated header file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include "Die.h"
 int main() {
-    Die d6(6, 1, 1, "white", "bone");
-    std::cout << "Hello, World!" << std::endl;
-    for (int i = 0; i < 20; i++)
-        cout << "The dice rolled " <<  d6.roll() << endl;
-        cout << "The dice rolled " <<  d6.roll() << endl;
+
+    Die dice1(6, 1, 1, "white", "bone");
+    Die dice2(6, 1, 1, "white", "bone");
+
     return 0;
 }
